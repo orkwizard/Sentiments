7bb20a1c23f8079c52acd406424bb85d0548756d
edgarosorio <edgarosorio@192.168.0.19>
2015-03-19 19:59:11 -0600

    New commit

--- Sentiments/src/com/sunset/sentiments/core/Transcriber.java
+++ Sentiments/src/com/sunset/sentiments/core/Transcriber.java
@@ -1,6 +1,8 @@
 package com.sunset.sentiments.core;
 
+import java.io.IOException;
 import java.io.InputStream;
+
 import edu.cmu.sphinx.api.Configuration;
 import edu.cmu.sphinx.api.SpeechResult;
 import edu.cmu.sphinx.api.StreamSpeechRecognizer;
@@ -17,11 +19,106 @@ public class Transcriber {
 	
 	public void loadConfig(String modelPath,String dictionary,String languageMode){
 		System.out.println("Loading models ......");
-		config = new Configuration();
 		config.setAcousticModelPath(modelPath);
 		config.setDictionaryPath(dictionary);
 		config.setLanguageModelPath(languageMode);
 		
+	}
+	
+	public void start(){
+		
+	}
+	
+	public Transcriber(){
+		//config = new Configuration();
+		
+	}
+	
+	public Transcriber(Configuration conf){
+		config = conf;
+	}
+	
+	public void Demo() throws Exception{
+		System.out.println("Loading models...");
+
+        Configuration configuration = new Configuration();
+
+        // Load model from the jar
+        configuration
+                .setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
+
+        // You can also load model from folder
+        // configuration.setAcousticModelPath("file:en-us");
+
+        configuration
+                .setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
+        configuration
+                .setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.dmp");
+
+        StreamSpeechRecognizer recognizer = new StreamSpeechRecognizer(
+                configuration);
+        InputStream stream = Transcriber.class
+                .getResourceAsStream("/edu/cmu/sphinx/demo/aligner/10001-90210-01803.wav");
+        stream.skip(44);
+
+        // Simple recognition with generic model
+        recognizer.startRecognition(stream);
+        SpeechResult result;
+        while ((result = recognizer.getResult()) != null) {
+
+            System.out.format("Hypothesis: %s\n", result.getHypothesis());
+
+            System.out.println("List of recognized words and their times:");
+            for (WordResult r : result.getWords()) {
+                System.out.println(r);
+            }
+
+            System.out.println("Best 3 hypothesis:");
+            for (String s : result.getNbest(3))
+                System.out.println(s);
+
+        }
+        recognizer.stopRecognition();
+
+        // Live adaptation to speaker with speaker profiles
+
+        stream = Transcriber.class
+                .getResourceAsStream("/edu/cmu/sphinx/demo/aligner/10001-90210-01803.wav");
+        stream.skip(44);
+
+        // Stats class is used to collect speaker-specific data
+        Stats stats = recognizer.createStats(1);
+        recognizer.startRecognition(stream);
+        while ((result = recognizer.getResult()) != null) {
+            stats.collect(result);
+        }
+        recognizer.stopRecognition();
+
+        // Transform represents the speech profile
+        Transform transform = stats.createTransform();
+        recognizer.setTransform(transform);
+
+        // Decode again with updated transform
+        stream = Transcriber.class
+                .getResourceAsStream("/edu/cmu/sphinx/demo/aligner/10001-90210-01803.wav");
+        stream.skip(44);
+        recognizer.startRecognition(stream);
+        while ((result = recognizer.getResult()) != null) {
+            System.out.format("Hypothesis: %s\n", result.getHypothesis());
+        }
+        recognizer.stopRecognition();
+		
+		
+	}
+	
+	public static void main(String[] args){
+		Transcriber t = new Transcriber();
+		try {
+			t.Demo();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 }
